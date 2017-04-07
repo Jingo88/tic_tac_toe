c755d40296d937a4ec7a10eada10c65334d52d40
Jason Ng <jcory.ng@gmail.com>
2017-04-07 12:06:16 -0400

    deleted all that s**t from before

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import HomeContainer from './containers/HomeContainer';
-import router from './router/router';
-
-render(
-	router,
-	document.getElementById('app')
-);
