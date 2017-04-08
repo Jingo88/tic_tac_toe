ec61969de9d5a7a244cdddb4f89886a2d206295b
Jason Ng <jcory.ng@gmail.com>
2017-04-08 13:57:56 -0400

    record showing

--- main.js
+++ main.js
@@ -3,25 +3,48 @@
 
     let board = document.querySelector('.board');
     let userForm = document.querySelector('.choice');
+    let reset = document.querySelector('.reset');
     let boardArr = [];
     let boardSize = 0;
     let userName = "";
     let pOne = 'X';
     let pTwo = 'O';
     let currPlayer = pOne;
+    let gameFinish = false;
+    let userInfo = document.querySelector('.info');
+    let record = {
+        wins: 0,
+        losses: 0,
+        ties: 0
+    }
 
     userForm.addEventListener('submit', function(event){
         event.preventDefault();
+        console.log(gameFinish);
 
-        if (boardArr.length !== 0){return;}
+        if (boardArr.length !== 0 || gameFinish){return;}
+
+        if (document.querySelector('.choice section>input[type="text"]').value === ""){
+            alert("Please Enter A Name");
+            return;
+        }
         
-        let data = event.target
+        let data = event.target;
 
-        userName = document.querySelector('.choice section>input[type="text"]').value;
+        if (userName !== document.querySelector('.choice section>input[type="text"]').value){
+            userName = document.querySelector('.choice section>input[type="text"]').value;    
+            userInfoCreate();
+        }
+
         boardSize = document.querySelector('input[name="size"]:checked').value;
-
-        createBoard(boardSize);
+        
+        createBoard(boardSize);        
     })
+
+    reset.addEventListener('click', function(){
+        console.log('a;sodifjasdfio')
+        resetGame()
+    });
 
     var createBoard = function(size){
 
@@ -32,7 +55,7 @@
             boardArr.push([])
 
             for (let j = 0; j<size; ++j){
-                let box = document.createElement('box');
+                let box = document.createElement('div');
                 box.className = "box";
                 
                 let dataNum = i.toString() + j.toString();
@@ -48,7 +71,7 @@
     }
 
     board.addEventListener('click', function(event){
-        if (boardSize === 0){return;}
+        if (boardSize === 0 || gameFinish){return;}
 
         let el = event.target;
 
@@ -62,6 +85,7 @@
         boardArr[row][col] = currPlayer;
 
         if (checkRow(row) || checkCol(col) || checkDOne() || checkDTwo()){
+            gameFinish = true;
             currPlayer === pOne ? alert(`CONGRATS ${userName} YOU WIN`) : alert(`YOU LOSE!!!`)
         }
 
@@ -106,4 +130,43 @@
         return true;
     }
 
+    var resetGame = function(){
+        console.log('reseting shit')
+        boardArr = [];
+        boardSize = 0;
+        while (board.firstChild){
+            console.log(board.firstChild)
+            board.removeChild(board.childNodes[0]);
+        }
+        currPlayer = pOne;
+        gameFinish = false;
+        console.log(board)
+    }
+
+    var userInfoCreate = function(){
+        let h3 = document.createElement('h3')
+        h3.innerHTML = "Welcome! " + userName;
+
+        let wins = document.createElement("h5")
+        wins.innerHTML = "Wins: " + record.wins;
+
+        let losses = document.createElement("h5")
+        losses.innerHTML = "Losses: " + record.losses;
+
+        let ties = document.createElement("h5")
+        ties.innerHTML = "Ties: " + record.ties;
+
+
+        userInfo.appendChild(h3);
+        userInfo.appendChild(wins);
+        userInfo.appendChild(losses);
+        userInfo.appendChild(ties);
+    }
+
 }());
+
+
+
+
+
+
